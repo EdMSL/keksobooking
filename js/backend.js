3f6ecb0cddb9c01d08b936ae044026110128b15d
EdMSL <eduard2011rus@gmail.com>
2018-12-21 13:30:49 +0700

    Переделано добавление сообщений при ошибках запросов

--- js/backend.js
+++ js/backend.js
@@ -8,7 +8,15 @@
   var REQUEST_STATUS_NOT_FOUND = 404;
   var REQUEST_STATUS_INTERNAL_STATUS_ERROR = 500;
 
-  function getXHR(onLoad, onError) {
+  var badRequestMessage = ': Неверный запрос';
+  var unauthorizedsMessage = ': Пользователь не авторизован';
+  var internalMessage = ': Неверный адрес для отправки';
+  var onLoadNotFoundMessage = ': Страница с объявлениями не найдена. Пожалуйста, обновите страницу или попробуйте позднее';
+  var onSaveNotFoundMessage = ': Страница не найдена. Пожалуйста, обновите страницу или попробуйте позднее';
+  var onLoadDefaultMessage = ': Произошла ошибка при загрузке списка похожих объявлений. Пожалуйста, обновите страницу или попробуйте позднее';
+  var onSaveDefaultMessage = ': Произошла ошибка при попытке сохранения объявления. Пожалуйста, обновите страницу или попробуйте позднее';
+
+  function getXHR(onLoad, onError, badRequestErrorMessage, unauthorizedsErrorMessage, notFoundErrorMessage, internalErrorMessage, defaultErrorMessage) {
     var xhr = new XMLHttpRequest();
     xhr.responseType = 'json';
 
@@ -19,19 +27,19 @@
           onLoad(xhr.response);
           break;
         case REQUEST_STATUS_BAD_REQUEST:
-          error = xhr.status + ': Неверный запрос';
+          error = xhr.status + badRequestErrorMessage;
           break;
         case REQUEST_STATUS_UNAUTHORIZEDS:
-          error = xhr.status + ': Пользователь не авторизован';
+          error = xhr.status + unauthorizedsErrorMessage;
           break;
         case REQUEST_STATUS_NOT_FOUND:
-          error = xhr.status + ': Страница не найдена. Пожалуйста, обновите страницу или попробуйте позднее';
+          error = xhr.status + notFoundErrorMessage;
           break;
         case REQUEST_STATUS_INTERNAL_STATUS_ERROR:
-          error = xhr.status + ': Неверный адрес';
+          error = xhr.status + internalErrorMessage;
           break;
         default:
-          error = xhr.status + ': Произошла ошибка при загрузке. Пожалуйста, обновите страницу или попробуйте позднее';
+          error = xhr.status + defaultErrorMessage;
       }
 
       if (error) {
@@ -52,13 +60,13 @@
   }
 
   function load(url, onLoad, onError) {
-    var xhr = getXHR(onLoad, onError);
+    var xhr = getXHR(onLoad, onError, badRequestMessage, unauthorizedsMessage, onLoadNotFoundMessage, internalMessage, onLoadDefaultMessage);
     xhr.open('GET', url);
     xhr.send();
   }
 
   function save(url, data, onLoad, onError) {
-    var xhr = getXHR(onLoad, onError);
+    var xhr = getXHR(onLoad, onError, badRequestMessage, unauthorizedsMessage, onSaveNotFoundMessage, internalMessage, onSaveDefaultMessage);
     xhr.open('POST', url);
     xhr.send(data);
   }
